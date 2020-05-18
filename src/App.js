695967ff4551056b61075ee8af4448a58f595c28
Fredrik <fredrik@eklundh.net>
2020-05-19 00:32:03 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import testImage from './img/fredrik_eklund.jpg';
 import './App.css';
 
 function App() {
+
+//const API_KEY = "RGAPI-5fbb0437-dcda-4380-bd06-e08c116c56c0"
+
+//const exampleRequest = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/1hithoodi?api_key=RGAPI-5fbb0437-dcda-4380-bd06-e08c116c56c0`;
+
+useEffect( async () => {
+  getApiRequest();
+}, []);
+
+
+const getApiRequest = async () => {
+  const response = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/1hithoodi?api_key=RGAPI-5fbb0437-dcda-4380-bd06-e08c116c56c0`);
+  const data = response.json();
+  console.log(data);
+}
+
+
   return (
     <div className="App">
      <h1>MY react app lel</h1>
