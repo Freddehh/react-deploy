854e2f7b56464f6bf39fe80fd9e8b0ca23fa9aac
RasmusOberg <44671186+RasmusOberg@users.noreply.github.com>
2020-05-19 17:35:22 +0200

    added api requests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import testImage from './img/fredrik_eklund.jpg';
 import './App.css';
+import championsFile from './data/champions.json';
 
 function App() {
 
@@ -9,17 +10,62 @@ function App() {
 
 //const exampleRequest = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/1hithoodi?api_key=RGAPI-5fbb0437-dcda-4380-bd06-e08c116c56c0`;
 
+let summonerID;
+let champArray = [];
+let test;
+
 useEffect(() => {
-  getApiRequest();
+  getSummonerIDFromName();
+  //getChampionMasteryFromID();
+  champArray = Object.entries(championsFile);
+  let array = champArray[0];
+  let arr2 = Object.entries(array);
+  test = Object.entries(arr2[1][1])
+  console.log(test[0][1].id)
+  console.log(test.length)
+  for(let i = 0; i < test.length; i++){
+    if(test[i][1].key == 34){
+      console.log(test[i][1].id)
+    }
+  }
 }, []);
 
-
-const getApiRequest = async () => {
-  console.log("1");
-  const response = await fetch("https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/this%20is%20yoloq?api_key=RGAPI-810f9b48-50c8-4e25-9d2d-a45e4e807cef");
+let summonerName = "thisisyoloq";
+let api_key = "RGAPI-a86283a2-57fd-491e-af18-0486f20c37c2";
+const getSummonerIDFromName = async () => {
+  //console.log("first");
+  const response = await fetch
+  ("https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summonerName+"?api_key="+api_key);
   const data = await response.json();
   console.log(data);
-  console.log("Hej");
+  summonerID = data.id;
+  console.log(data.name);
+  console.log(data.id);
+  const response1 = await fetch
+  ("https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/"+summonerID+"?api_key="+api_key);
+  const allChampions = await response1.json();
+  console.log(allChampions);
+  console.log(allChampions.length);
+  for(let i = 0; i < allChampions.length; i++){
+    let currentId = allChampions[i].championId;
+    let currentPts = allChampions[i].championPoints;
+    console.log(allChampions[i].championId);
+    for(let j = 0; j < test.length; j++){
+      if(test[j][1].key == currentId){
+        console.log(test[j][1].id);
+      }
+    }
+  }
+}
+
+const getChampionMasteryFromID = async () => {
+  let t1 = "AqxFkaEqZ_iL_dYU4KKB3NCRGjhK8LxgDu4J3cKrS2QH8YA";
+  const response1 = await fetch
+  ("https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/"+summonerID+"?api_key="+api_key);
+  const allChampions = await response1.json();
+  console.log(allChampions);
+  console.log(allChampions[0].championPoints);
+  
 }
 
 
