34f0094e9473995ca1e81a417e5e6d6c43daccb8
RasmusOberg <44671186+RasmusOberg@users.noreply.github.com>
2020-05-19 12:51:53 +0200

    api added

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ function App() {
 
 //const exampleRequest = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/1hithoodi?api_key=RGAPI-5fbb0437-dcda-4380-bd06-e08c116c56c0`;
 
-useEffect( async () => {
+useEffect(() => {
   getApiRequest();
 }, []);
 
 
 const getApiRequest = async () => {
-  console.log("Hej");
-  const response = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/1hithoodi?api_key=RGAPI-5fbb0437-dcda-4380-bd06-e08c116c56c0`);
-  const data = response.json();
+  console.log("1");
+  const response = await fetch("https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/this%20is%20yoloq?api_key=RGAPI-810f9b48-50c8-4e25-9d2d-a45e4e807cef");
+  const data = await response.json();
   console.log(data);
   console.log("Hej");
 }
